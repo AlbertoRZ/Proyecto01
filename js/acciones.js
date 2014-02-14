09f7385375c826bd666293959fef5470e53cee61
Alberto Rivera <albertorivera581@gmail.com>
2014-02-14 12:27:32 -0600

    Cambio de error
    Nada

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	document.addEventListener("deviceready",function(){
 $('#B1').tap(function(){
-	navegator.notification.beep(1);//Es un navegador porque es una aplicación web
+	navigator.notification.beep(1);//Es un navegador porque es una aplicación web
 });//tap de B1
 	});//device ready
 });//ready
